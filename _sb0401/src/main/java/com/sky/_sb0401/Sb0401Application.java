c255bdf556f19afc9c557660975feffa6ca1f15b
Jiwoong-Jung <jjw981119@naver.com>
2024-04-01 10:55:15 +0900

    first commit

---
+++ _sb0401/src/main/java/com/sky/_sb0401/Sb0401Application.java
@@ -0,0 +1,13 @@
+package com.sky._sb0401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb0401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb0401Application.class, args);
+    }
+
+}
---
+++ _sb0401/src/test/java/com/sky/_sb0401/Sb0401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky._sb0401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb0401ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
