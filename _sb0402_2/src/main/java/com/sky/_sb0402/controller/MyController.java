e9c6c755839ebf3505ebfc412d56af7d0e525f73
Jiwoong-Jung <jjw981119@naver.com>
2024-04-02 11:52:01 +0900

    thymeleaf 적용

--- _sb0402_2/src/main/java/com/sky/_sb0402/controller/MyController.java
+++ _sb0402_2/src/main/java/com/sky/_sb0402/controller/MyController.java
@@ -1,13 +1,15 @@
 package com.sky._sb0402.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
 
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("name", "이순신");
         return "root";
     }
 }
