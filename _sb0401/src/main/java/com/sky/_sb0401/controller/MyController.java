5812eedcbe0f5d08eee7c0329fdb5dc4c77c096b
Jiwoong-Jung <jjw981119@naver.com>
2024-04-04 10:38:31 +0900

    JPA 기초

--- _sb0401/src/main/java/com/sky/_sb0401/controller/MyController.java
+++ _sb0401/src/main/java/com/sky/_sb0401/controller/MyController.java
@@ -3,26 +3,20 @@ package com.sky._sb0401.controller;
 import com.sky._sb0401.entity.Memo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-@Controller
+@RestController
 public class MyController {
 
-    @PersistenceContext
-    EntityManager em;
+
 
     @GetMapping("/")
-    @Transactional
     public String root() {
-        Memo memo = Memo.builder()
-                .mno(3L)
-                .memoText("이것은 EM 테스트22222")
-                .build();
-        em.merge(memo);
-        em.flush();
+
         return "main";
     }
 
