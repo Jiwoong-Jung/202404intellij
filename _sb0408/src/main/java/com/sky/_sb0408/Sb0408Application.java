a1a398c7000999c94a79fca122eb2d2a36e2a0ed
Jiwoong-Jung <jjw981119@naver.com>
2024-04-08 14:47:22 +0900

    sp5 chap11

---
+++ _sb0408/src/main/java/com/sky/_sb0408/Sb0408Application.java
@@ -0,0 +1,13 @@
+package com.sky._sb0408;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb0408Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb0408Application.class, args);
+    }
+
+}
