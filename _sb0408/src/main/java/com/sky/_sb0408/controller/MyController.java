b999020e658bf61ee75291853dc5d31ec5c668b8
Jiwoong-Jung <jjw981119@naver.com>
2024-04-08 15:37:39 +0900

    sp5 chap11

--- _sb0408/src/main/java/com/sky/_sb0408/controller/MyController.java
+++ _sb0408/src/main/java/com/sky/_sb0408/controller/MyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MyController {
 
     @GetMapping("/")
-    public String index() {
+    public String main() {
         return "main";
     }
 }
---
+++ _sb0408/src/main/java/com/sky/_sb0408/spring/DuplicateMemberException.java
@@ -0,0 +1,9 @@
+package com.sky._sb0408.spring;
+
+public class DuplicateMemberException extends RuntimeException {
+
+	public DuplicateMemberException(String message) {
+		super(message);
+	}
+
+}
