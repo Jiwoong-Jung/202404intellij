82be55d6f241f09382c1d785d5608a4c8dfac71b
Jiwoong-Jung <jjw981119@naver.com>
2024-04-09 14:49:18 +0900

    MyController 삭제

--- _sb0408/src/main/java/com/sky/_sb0408/controller/RegisterController.java
+++ _sb0408/src/main/java/com/sky/_sb0408/controller/RegisterController.java
@@ -22,8 +22,7 @@ public class RegisterController {
 
 	@PostMapping("/step2")
 	public String handleStep2(
-			@RequestParam(value = "agree", defaultValue = "false") Boolean agree,
-			Model model) {
+			@RequestParam(value = "agree", defaultValue = "false") Boolean agree, Model model) {
 		if (!agree) {
 			return "register/step1";
 		}
--- _sb0408/src/main/webapp/WEB-INF/views/main.jsp
+++ _sb0408/src/main/webapp/WEB-INF/views/main.jsp
@@ -7,8 +7,6 @@
 </head>
 <body>
     <p>환영합니다.</p>
-    <h2>${value1}</h2>
-    <h2>${value2}</h2>
     <p><a href="<c:url value="/register/step1" />">[회원 가입하기]</a>
 </body>
 </html>
