61136a20f1f74cb953d6f579e89d26aca0ae8d63
Jiwoong-Jung <jjw981119@naver.com>
2024-04-11 14:08:02 +0900

    first commit

---
+++ _sb0411/src/main/java/com/sky/_sb0411/Sb0411Application.java
@@ -0,0 +1,13 @@
+package com.sky._sb0411;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb0411Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb0411Application.class, args);
+    }
+
+}
