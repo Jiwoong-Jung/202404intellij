08638d1d39f3e4625d6b8a5a6fb88fdb30a25636
Jiwoong-Jung <jjw981119@naver.com>
2024-04-11 15:41:48 +0900

    problem 만들기

--- _sb0411/src/main/java/com/sky/_sb0411/problem/ProblemController.java
+++ _sb0411/src/main/java/com/sky/_sb0411/problem/ProblemController.java
@@ -20,9 +20,10 @@ public class ProblemController {
 
         Question q1 = new Question("당신의 역할은 무엇입니까?",
                 Arrays.asList("백엔드", "프론트", "DBA", "웹디자인"));
-        Question q1 = new Question("당신의 역할은 무엇입니까?",
-                Arrays.asList("백엔드", "프론트", "DBA", "웹디자인"));
-        model.addAttribute("question", q1);
+        Question q2 = new Question("많이 사용하는 개발도구는 무엇입니까?",
+                Arrays.asList("이클립스", "인텔리J", "서브라임"));
+        model.addAttribute("questions", Arrays.asList(q1, q2));
+
         return "problem/problemForm";
     }
 
