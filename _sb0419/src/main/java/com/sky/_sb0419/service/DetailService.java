e41b8b2687f7d9d7389d5c8101dbddd92936b4ea
Jiwoong-Jung <jjw981119@naver.com>
2024-04-19 11:41:53 +0900

    jpa 게시판

---
+++ _sb0419/src/main/java/com/sky/_sb0419/service/DetailService.java
@@ -0,0 +1,8 @@
+package com.sky._sb0419.service;
+
+
+import com.sky._sb0419.entity.Notice;
+
+public interface DetailService {
+    Notice detail(Long seq);
+}
---
+++ _sb0419/src/main/java/com/sky/_sb0419/service/NoticeService.java
@@ -0,0 +1,8 @@
+package com.sky._sb0419.service;
+
+
+import com.sky._sb0419.entity.Notice;
+
+public interface NoticeService {
+    void saveNotice(Notice notice);
+}
